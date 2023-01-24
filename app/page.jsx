b70729d9971b920cd8c7039812daae34eddd7720
Samuel Garcia <samfrontdev@gmail.com>
2023-01-24 15:21:09 -0500

    Basic functions

--- app/page.jsx
+++ app/page.jsx
@@ -1,91 +1,92 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import { useRef, useEffect } from "react";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const stripRef = useRef(null);
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "environment",
+          width: 500,
+          height: 625,
+        },
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.log("error:", err);
+      });
+  };
+
+  const paintToCanvas = () => {
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    let ctx = photo.getContext("2d");
+
+    const width = 500;
+    const height = 625;
+    photo.width = width;
+    photo.height = height;
+
+    return setInterval(() => {
+      ctx.drawImage(video, 0, 0, photo.width, photo.height);
+      const scannedImage = ctx.getImageData(0, 0, photo.width, photo.height);
+      const scannedData = scannedImage.data;
+      const pixelationFactor = 6;
+      for (let y = 0; y < photo.height; y += pixelationFactor) {
+        for (let x = 0; x < photo.width; x += pixelationFactor) {
+          // extracting the position of the sample pixel
+          const pixelIndexPosition = (x + y * photo.width) * 4;
+          // drawing a square replacing the current pixels
+          ctx.fillStyle = `rgba(
+            ${scannedData[pixelIndexPosition]},
+            ${scannedData[pixelIndexPosition + 1]},
+            ${scannedData[pixelIndexPosition + 2]},
+            ${scannedData[pixelIndexPosition + 3]}
+            )`;
+          ctx.fillRect(x, y, pixelationFactor, pixelationFactor);
+        }
+      }
+    }, 200);
+  };
+  const takePhoto = () => {
+    let photo = photoRef.current;
+    let strip = stripRef.current;
+
+    const data = photo.toDataURL("image/jpeg");
+
+    const link = document.createElement("a");
+    link.href = data;
+    link.setAttribute("download", "myWebcam");
+    link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+    strip.insertBefore(link, strip.firstChild);
+  };
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.jsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h1>AEX</h1>
+      <button onClick={() => takePhoto()}>Take a photo</button>
+      <video hidden ref={videoRef} onCanPlay={() => paintToCanvas()} />
+      <canvas ref={photoRef} />
+      <div className="photo-booth">
+        <div ref={stripRef} className="strip" />
       </div>
     </main>
-  )
+  );
 }
