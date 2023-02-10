ba78b350505d5570d201b1b16dffb282ac4d91e6
Samuel Garcia <samfrontdev@gmail.com>
2023-02-10 11:08:58 -0500

    Defined 1/1 aspect ratio and Webcam

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ import styles from "./page.module.css";
 import Gallery from "./Gallery";
 import Menu from "./Menu";
 import { FiRefreshCcw } from "react-icons/fi";
+import Webcam from "react-webcam";
 
 export default function Home() {
   const initialImage = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAQSURBVHgBAQUA+v8AAAAAAAAFAAFkeJU4AAAAAElFTkSuQmCC`;
@@ -47,6 +48,9 @@ export default function Home() {
     window.localStorage.setItem("AEXPHOTO", JSON.stringify(photo));
     // console.log(photo)
   };
+  useEffect(() => {
+    paintToCanvas;
+  }, [videoRef]);
 
   const getVideo = () => {
     navigator.mediaDevices
@@ -54,8 +58,8 @@ export default function Home() {
         audio: false,
         video: {
           facingMode: facingMode,
-          width: { ideal: 1080 },
-          height: { ideal: 1350 },
+          width: { min: 720 },
+          height: 900,
         },
       })
       .then((stream) => {
@@ -88,33 +92,32 @@ export default function Home() {
     let video = videoRef.current;
     let photo = photoRef.current;
     let ctx = photo.getContext("2d");
-
-    const width = 1080;
-    const height = 1350;
-    photo.width = width;
-    photo.height = height;
-
+    photo.width = 720;
+    photo.height = 720;
+    console.log(video.video);
     return setInterval(() => {
-      ctx.drawImage(video, 0, 0, photo.width, photo.height);
+      ctx.drawImage(video.video, 0, 0, photo.width, photo.height);
+
+      console.log(ctx);
       const scannedImage = ctx.getImageData(0, 0, photo.width, photo.height);
       const scannedData = scannedImage.data;
       const pixelationFactor = 4;
-      green(scannedData);
-      for (let y = 0; y < photo.height; y += pixelationFactor) {
-        for (let x = 0; x < photo.width; x += pixelationFactor) {
-          // extracting the position of the sample pixel
-          const pixelIndexPosition = (x + y * photo.width) * 4;
-          // drawing a square replacing the current pixels
-          ctx.fillStyle = `rgba(
-            ${scannedData[pixelIndexPosition]},
-            ${scannedData[pixelIndexPosition + 1]},
-            ${scannedData[pixelIndexPosition + 2]},
-            ${scannedData[pixelIndexPosition + 3]}
-            )`;
-          ctx.fillRect(x, y, pixelationFactor, pixelationFactor);
+      // green(scannedData);
+      if (pixelationFactor !== 0) {
+        for (let y = 0; y < photo.height; y += pixelationFactor) {
+          for (let x = 0; x < photo.width; x += pixelationFactor) {
+            const pixelIndexPosition = (x + y * photo.width) * 4;
+            ctx.fillStyle = `rgba(
+              ${scannedData[pixelIndexPosition]},
+              ${scannedData[pixelIndexPosition + 1]},
+              ${scannedData[pixelIndexPosition + 2]},
+              ${scannedData[pixelIndexPosition + 3]}
+              )`;
+            ctx.fillRect(x, y, pixelationFactor, pixelationFactor);
+          }
         }
       }
-    }, 200);
+    }, 100);
   };
 
   const takePhoto = () => {
@@ -133,27 +136,28 @@ export default function Home() {
     setPhotos(deletedPhoto);
     photoLocalStorage(deletedPhoto);
   };
+  const videoConstrains = {
+    width: 720,
+    height: 720,
+    aspectRatio: 1,
+  };
   return (
     <main className={styles.main}>
       <h1>AEX</h1>
       <div className={styles.display}>
-        <div className={styles.camera}>
-          <video hidden ref={videoRef} onCanPlay={() => paintToCanvas()} />
-          <canvas className={styles.facecanvas} ref={photoRef} />
+        <div className={styles.cameraContainer}>
+          <div className={styles.camera}>
+            {/* <video hidden ref={videoRef} onCanPlay={() => paintToCanvas()} /> */}
+            <Webcam
+              ref={videoRef}
+              videoConstraints={videoConstrains}
+              onPlay={() => paintToCanvas()}
+            />
+            <canvas className={styles.facecanvas} ref={photoRef} />
+          </div>
         </div>
       </div>
       <div className={styles.actions}>
-        <button
-          className={styles.cameraswitch}
-          onClick={() => toggleFacingMode()}
-        >
-          <FiRefreshCcw style={{ fontSize: "35px" }} />
-        </button>
-        <button
-          className={styles.cameratrigger}
-          onClick={() => takePhoto()}
-          ref={triggerRef}
-        ></button>
         <div
           className={styles.galleryCover}
           id="empty-div"
@@ -162,6 +166,17 @@ export default function Home() {
         >
           <img src={photos[0]} alt="" />
         </div>
+        <button
+          className={styles.cameratrigger}
+          onClick={() => takePhoto()}
+          ref={triggerRef}
+        ></button>
+        <button
+          className={styles.cameraswitch}
+          onClick={() => toggleFacingMode()}
+        >
+          <FiRefreshCcw style={{ fontSize: "35px" }} />
+        </button>
       </div>
       {modal && (
         <div className={styles.modalcontainer}>
