427db53c995921854a7beb6442e8afc174c2c0ad
Samuel Garcia <samfrontdev@gmail.com>
2023-02-11 16:56:33 -0500

    functional swipe

--- app/Gallery.jsx
+++ app/Gallery.jsx
@@ -1,109 +1,44 @@
-"use-client";
-import { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./gallery.module.css";
 import { FiDownload, FiArrowLeft, FiTrash2 } from "react-icons/fi";
-import PhotoSwipeLightbox from "photoswipe/lightbox";
-import "photoswipe/style.css";
 
-export default function gallery(props) {
-  const [selectedImage, setSelectedImage] = useState(null);
-  useEffect(() => {
-    let lightbox = new PhotoSwipeLightbox({
-      gallery: "#" + props.galleryID,
-      children: "a",
-      pswpModule: () => import("photoswipe"),
-    });
-    lightbox.init();
+import "swiper/css";
 
-    return () => {
-      lightbox.destroy();
-      lightbox = null;
-    };
-  }, []);
-
-  const handleSelectedImage = (src) => {
-    setSelectedImage(src);
-  };
-  const namePhoto = () => {
-    const randomNumber = Math.floor(Math.random() * 100000);
-    const randomLetters = Math.random().toString(36).substring(7);
-    return `AEX${randomNumber}-${randomLetters}.jpg`;
-  };
-  const deleteFromGallery = () => {
-    props.delete(selectedImage);
-    setSelectedImage(null);
-  };
+const Gallery = (props) => {
   return (
     <div className={styles.galleryContainer}>
-      <div className={styles.gallery}>
-        <div
-          className={styles.galleryGrid || "pswp-gallery"}
-          id={props.galleryID}
-        >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+          position: "fixed",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Swiper navigation={true}>
           {props.srcs.map((src, index) => (
-            <img
-              data-pswp-width={100}
-              data-pswp-height={100}
-              src={src}
-              alt=""
-            />
+            <SwiperSlide>
+              <img src={src} alt={src} style={{ width: "100%" }} />
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
-      {/* {selectedImage ? (
-          <div className={styles.selectedImageContainer}>
-            <img src={selectedImage} alt="" />
-
-          </div>
-        ) : (
-          <div className={styles.galleryGrid}>
-            {props.srcs.map((src) => {
-              return (
-                <img
-                  src={src}
-                  onClick={() => handleSelectedImage(src)}
-                  key={namePhoto()}
-                />
-              );
-            })}
-          </div>
-        )} */}
-      {selectedImage ? (
-        <div className={styles.galleryMenu}>
-          <button
-            className={styles.backButton}
-            onClick={() => setSelectedImage(null)}
-          >
-            <FiArrowLeft style={{ fontSize: "35px" }} />
-          </button>
-          <a href={selectedImage} download={namePhoto()}>
-            <FiDownload style={{ fontSize: "35px" }} />
-          </a>
-          <button
-            className={styles.backButton}
-            onClick={() => deleteFromGallery()}
-          >
-            <FiTrash2 style={{ fontSize: "35px" }} />
-          </button>
-        </div>
-      ) : (
-        <div className={styles.galleryMenu}>
-          <button
-            className={styles.backButton}
-            onClick={() => props.closeGallery()}
-          >
-            <FiArrowLeft style={{ fontSize: "35px" }} />
-          </button>
-          <h3>GALLERY</h3>
-        </div>
-      )}
+      <div className={styles.galleryMenu}>
+        <button className={styles.backButton}>
+          <FiArrowLeft style={{ fontSize: "35px" }} />
+        </button>
+        <a href="">
+          <FiDownload style={{ fontSize: "35px" }} />
+        </a>
+        <button className={styles.backButton}>
+          <FiTrash2 style={{ fontSize: "35px" }} />
+        </button>
+      </div>
     </div>
-    // <div className={styles.gallery}>
-    //   {srcs.length(
-    //     srcs.map((src) => {
-    //       return <img src={src} key={src} />;
-    //     })
-    //   )}
-    // </div>
   );
-}
+};
+
+export default Gallery;
